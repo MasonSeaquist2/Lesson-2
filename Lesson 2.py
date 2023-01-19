ca8bf0f7a98f6b9b8e195bfaa85efffd32f70ece
MasonSeaquist2 <122318779+MasonSeaquist2@users.noreply.github.com>
2023-01-19 16:07:59 -0600

    Add files via upload

---
+++ Lesson 2.py
@@ -0,0 +1,24 @@
+import random
+
+# Math
+
+
+print(4 + 4)
+print(4.4 + 4)
+fress = random.randint(1, 10)
+if fress % 2 == 0:
+    print("Heads")
+else:
+    print("Tails")
+
+print((5 / 1) ** 20)
+print(5 == 3)
+print(5 == 5)
+
+list1 = list[2, 3, 4, 5]
+print(list1)
+
+count = 1
+print(count)
+count += 1
+print(count)
